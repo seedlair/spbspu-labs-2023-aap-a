4529364a9a3a6a4fd94642ff65396ec6543afa6f
seedlair <poleleyko.ivan@gmail.com>
2024-03-19 02:24:30 +0300

    .

---
+++ poleleyko.ivan/P4/read_matrix.hpp
@@ -0,0 +1,14 @@
+#ifndef READ_MATRIX_HPP
+#define READ_MATRIX_HPP
+
+#include <fstream>
+#include <cstddef>
+
+namespace poleleyko
+{
+
+ void read_matrix(std::istream& input_stream, int* array, size_t size_of_array);
+
+}
+
+#endif
---
+++ poleleyko.ivan/P4/tasks.hpp
@@ -0,0 +1,15 @@
+#ifndef TASKS_HPP
+#define TASKS_HPP
+
+#include <cstddef>
+
+namespace poleleyko
+{
+
+  void spiral_matrix_transformation(int* matrix, size_t count_of_rows, size_t count_of_cols);
+
+  int count_row_not_same_numbers(int* matrix, size_t count_of_rows, size_t count_of_cols);
+
+}
+
+#endif
