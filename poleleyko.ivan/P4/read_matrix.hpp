a87770eead968331b7a2035dd9144dfd480a579c
seedlair <poleleyko.ivan@gmail.com>
2024-03-19 03:58:01 +0300

    .

--- poleleyko.ivan/P4/read_matrix.hpp
+++ poleleyko.ivan/P4/read_matrix.hpp
@@ -7,7 +7,7 @@
 namespace poleleyko
 {
 
- void read_matrix(std::istream& input_stream, int* array, size_t size_of_array);
+  void read_matrix(std::istream& input_stream, int* array, size_t size_of_array);
 
 }
 
