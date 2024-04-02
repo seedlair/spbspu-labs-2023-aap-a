3d0abf990d1daa92fecb6f1b497cc77fdb3e7f91
seedlair <poleleyko.ivan@gmail.com>
2024-04-02 16:37:32 +0300

    da

--- poleleyko.ivan/P4/tasks.hpp
+++ poleleyko.ivan/P4/tasks.hpp
@@ -5,11 +5,8 @@
 
 namespace poleleyko
 {
-
   void spiral_matrix_transformation(int* matrix, size_t count_of_rows, size_t count_of_cols);
-
   int count_row_not_same_numbers(int* matrix, size_t count_of_rows, size_t count_of_cols);
-
 }
 
 #endif
